fc3a895a7c2f4a29fd4a1a8d1e241011bc5ca2fb
João Victor Araujo Garcia <joaovictor.a.garcia@gmail.com>
2023-07-27 23:13:55 -0300

    Fix Project
    Upload arquivos dependentes

---
+++ Models/Habilidades.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace _7DaysOfCode_C_.Models;
+
+class Habilidades
+{
+    [JsonPropertyName("ability")]
+    public List<Habilidade> ListaHabilidade { get; set; }
+
+    public void ExibirDetalhes()
+    {
+        foreach (var item in ListaHabilidade)
+        {
+            Console.WriteLine(item.Nome);
+        }
+    }
+}
---
+++ Models/Mascote.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace _7DaysOfCode_C_.Models;
+
+internal class Mascote
+{
+    [JsonPropertyName("name")]
+    public string Nome {get;set;}
+    [JsonPropertyName("height")]
+    public int Altura { get; set; }
+    [JsonPropertyName("weight")]
+    public int Peso { get;set;}
+    [JsonPropertyName("abilities")]
+    public Habilidades Habilidades { get;set;}
+
+    public void ExibirDetalhes()
+    {
+        Console.WriteLine($"Nome Pokemon: {Nome} \nAltura: {Altura} \nPeso: {Peso} \nHabilidades: {Habilidades.ExibirDetalhes} ");
+    }
+}
