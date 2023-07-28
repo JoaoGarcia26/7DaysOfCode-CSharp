b3fea52a259d7d0505ac4c5f94fb5a238d4568ac
João Victor Araujo Garcia <joaovictor.a.garcia@gmail.com>
2023-07-27 23:12:54 -0300

    #7DaysOfCode - C# - 1/7
    Conexão com uma API Rest

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using _7DaysOfCode_C_.Models;
+using RestSharp;
+using System.Text.Json;
+
+static void InvocarGet(string endpoint)
+{
+    var client = new RestClient($"https://pokeapi.co/api/v2/pokemon/{endpoint}");
+    var request = new RestRequest("", Method.Get);
+    var response = client.Execute(request);
+    
+    if (response.StatusCode == System.Net.HttpStatusCode.OK)
+    {
+        Console.WriteLine(response.Content);
+    } else
+    {
+        Console.WriteLine(response.ErrorMessage);
+    }
+}
+
+InvocarGet("pikachu");
+
+
+
